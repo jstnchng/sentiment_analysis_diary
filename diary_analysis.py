c7d2ecf469c68459ff0c85a86d5e1f7954028c5a
Justin Chng <jchang1397@gmail.com>
2017-10-22 22:04:51 -0700

    reworked tons of stuff, and added a todo

--- diary_analysis.py
+++ diary_analysis.py
@@ -4,33 +4,34 @@ import os, math
 import re
 import matplotlib.pyplot as plt
 import numpy as np
+from datetime import datetime
 
 months = {1:'January', 2:'February', 3:'March', 4:'April', 5:'May', 6:'June', 7:'July', 8:'August', 9:'September', 10:'October', 11:'November', 12:'December'}
 base_year = 2012
-start_year = 2
-end_year = 7
+num_years = 6
 start_month = 10
 end_month = 9
 
-def tf(diary):
+def get_wf(diary):
     wf = {}
     #  for word in diary.noun_phrases:
     for word in diary.words:
         if word in wf:
-            wf[word] = wf.get(word)+1
+            wf[word] += 1
         else:
             wf[word] = 1
     return wf
 
 def verify_clean_data(raw_diary):
-    for i in range(start_year, end_year):
+    for i in range(num_years):
         for month in months.values():
-            month_year = month + ' 201' + str(i)
+            month_year = month + ' ' + str(i+base_year)
             if raw_diary.count(month_year) > 1:
                 print month_year
                 raise ValueError(month_year)
 
-def clean_words(diary):
+def clean_words(raw_diary):
+    diary = TextBlob(raw_diary)
     stop = set(stopwords.words('english'))
     cleaned_diary = []
     for word in diary.words:
@@ -38,17 +39,27 @@ def clean_words(diary):
             cleaned_diary.append(re.sub(r'\W+', '', word.lower()).title())
     return TextBlob(' '.join(cleaned_diary))
 
-def plot_wf(diary, number):
-    word_frequency = tf(diary)
+def write_to_file(filename, csv):
+    f = open(filename, 'w+')
+    f.write(csv)
+
+def plot_wf(diary, number, time):
+    word_frequency = get_wf(diary)
 
     sorted_words = sorted(word_frequency.items(), key=lambda x: x[1], reverse=True)
     words, scores = zip(*sorted_words[:number])
+    csv = ''
     for i, (word, score) in enumerate(sorted_words[:number]):
-        print "\tIndex: ", i ," Word: ", word, " TF: ", round(score,5), " %: ", float(score)/len(diary.words)
-    xpos = np.arange(len(words))
-    plt.bar(xpos, scores)
-    plt.xticks(xpos, words)
-    plt.show()
+        tf = round(score,5)
+        percentage = str(round(float(score)/len(diary.words)*100,5)) + '%'
+        print "\tIndex: ", i ," Word: ", word, " TF: ", tf, " %: ", percentage
+        csv += str(word) + ',' + str(tf) + ',' + percentage + '\n'
+    filename = 'wf_' + time + datetime.now().strftime('%Y-%m-%d-%H-%M-%s') + '.csv'
+    write_to_file(filename, csv)
+    #  xpos = np.arange(len(words))
+    #  plt.bar(xpos, scores)
+    #  plt.xticks(xpos, words)
+    #  plt.show()
 
 def get_month_year(index):
     if index < 3:
@@ -59,76 +70,85 @@ def get_month_year(index):
         month = months.get(remaining%12+1)
         return str(month) + ' ' + str(year)
 
-def split_by_year(diary):
+def split_by_year(raw_diary):
     diary_by_year = []
-    for i in range(start_year+1,end_year+1):
-        idx = diary.find('January 201' + str(i))
-        diary_by_year.append(diary[:idx])
-        diary = diary[idx:]
-    diary_by_year.append(diary)
-    assert len(diary_by_year) == end_year-start_year+1
+    for i in range(1,num_years):
+        year = base_year+i-1
+        idx = raw_diary.find('January ' + str(year))
+        diary_by_year.append( (str(year+1), clean_words(raw_diary[:idx])) )
+        raw_diary = raw_diary[idx:]
+    diary_by_year.append((str(base_year+num_years-1), clean_words(raw_diary)))
     print('Number of years analyzed: ' + str(len(diary_by_year)))
+    assert len(diary_by_year) == num_years
     return diary_by_year
 
-def split_by_month(diary):
+def split_by_month(raw_diary):
     diary_by_month = []
-    num_months = (end_year-start_year)*12 + (end_month-start_month)
-    #  print (num_months)
+    num_months = (num_years-1)*12 + (end_month-start_month)
     for i in range(1,num_months+1):
-        #  print(get_month_year(i))
-        idx = diary.find(get_month_year(i))
-        diary_by_month.append(diary[:idx])
-        diary = diary[idx:]
-    diary_by_month.append(diary)
+        month_year = get_month_year(i-1)
+        idx = raw_diary.find(get_month_year(i))
+        diary_by_month.append((month_year, clean_words(raw_diary[:idx])))
+        raw_diary = raw_diary[idx:]
+    diary_by_month.append( (get_month_year(num_months), clean_words(raw_diary)) )
     print('Number of months analyzed: ' + str(len(diary_by_month)))
     return diary_by_month
 
-def analyze_sentiment_overall(diary):
+def analyze_sentiment_all(diary):
     overall_sentiment = diary.sentiment
-    print('Sentiment Analysis overall: ' + str(overall_sentiment.polarity))
+    polarity = str(overall_sentiment.polarity)
+    subjectivity = str(overall_sentiment.subjectivity)
+    print('Sentiment Analysis overall: ' + polarity)
+    csv = 'Polarity, Subjectivity\n'
+    csv += polarity + ',' + subjectivity + '\n'
+    filename = 'sentiment_overall' + datetime.now().strftime('%Y-%m-%d-%H-%M-%s') + '.csv'
+    write_to_file(filename, csv)
     return overall_sentiment
 
-def analyze_sentiment_year(raw_diary):
-    years = []
-    for i, y in enumerate(diary_by_year):
-        year = TextBlob(y)
-        idx = i + base_year
-        year_sentiment = year.sentiment
-        print('Sentiment Analysis for ' + str(idx) + ': ' + str(year_sentiment.polarity))
-        years.append((idx, year_sentiment.polarity, year.sentiment.subjectivity))
-    return years
+def analyze_sentiment_list(time, diary_list):
+    sentiments = []
+    csv = time + ', Polarity, Subjectivity\n'
+    filename = 'sentiment_by_' + time + datetime.now().strftime('%Y-%m-%d-%H-%M-%s') + '.csv'
+    for diary in diary_list:
+        text = diary[1]
+        text_id = diary[0]
+        sentiment = text.sentiment
+        polarity = str(sentiment.polarity)
+        subjectivity = str(sentiment.subjectivity)
+        print('Sentiment Analysis for ' + text_id + ': ' + polarity)
+        csv += text_id + ',' + polarity + ',' + subjectivity + '\n'
+        sentiments.append((text_id, polarity, subjectivity))
+    write_to_file(filename, csv)
+    return sentiments
 
-def analyze_sentiment_month(raw_diary):
-    months = []
-    for i, m in enumerate(diary_by_month):
-        month = TextBlob(m)
-        idx = get_month_year(i)
-        month_sentiment = month.sentiment
-        print('Sentiment Analysis for ' + str(idx) + ': ' + str(month_sentiment.polarity))
-        months.append((idx, month_sentiment.polarity, month.sentiment.subjectivity))
-    return months
+#  def get_wf_for_word(word, wfs):
+    #  wf_word = []
+    #  for wf in wfs:
+        #  score = wf.get(word)
+        #  tf = round(score,5)
+        #  percentage = str(round(float(score)/len(wf.words)*100,5)) + '%'
+        #  print "\tIndex: ", i ," Word: ", word, " TF: ", tf, " %: ", percentage
+        #  csv += str(word) + ',' + str(tf) + ',' + percentage + '\n'
+    #  return
 
 if __name__ == '__main__':
     with open('/Users/jchang1397/Downloads/diary', 'r') as d:
     #  with open('test', 'r') as d:
         raw_diary = d.read().decode('utf-8')
     verify_clean_data(raw_diary)
-    diary = clean_words(TextBlob(raw_diary))
+    diary = clean_words(raw_diary)
 
-    #  plot_wf(diary, 100)
+    analyze_sentiment_all(diary)
+    plot_wf(diary, 100, 'all')
 
-    #  analyze_sentiment_overall(diary)
-
-    #  diary_by_year = split_by_year(raw_diary)
-    #  analyze_sentiment_year(diary_by_year)
+    diary_by_year = split_by_year(raw_diary)
+    analyze_sentiment_list('Year', diary_by_year)
+    analyze_sentiment_year(diary_by_year)
 
     diary_by_month = split_by_month(raw_diary)
-    #  print len(diary_by_month)
-    #  for m in diary_by_month:
-        #  print ('\n')
-        #  print ('split\n')
-        #  print (m)
-    #  print( str(diary_by_month[1]))
-    analyze_sentiment_month(diary_by_month)
+    analyze_sentiment_list('Month', diary_by_month)
 
-    #  diary_by_month = split_by_month(raw_diary)
+    #  wf_by_month = []
+    #  for month in diary_by_month:
+        #  wf_by_month.append(get_wf(month))
+
